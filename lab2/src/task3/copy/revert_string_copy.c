575c8394007088674e9f647f390eec6ae583345f
jul-andreevna <yuliaaleks28070@gmail.com>
2020-05-06 12:09:33 +0000

    added revert_string_copy.c and revert_string_copy.h

---
+++ lab2/src/task3/copy/revert_string_copy.c
@@ -0,0 +1,21 @@
+#include "revert_string.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void RevertString(char *str)
+{
+	// your code here
+    int size, i; //инициализируем переменную size - размер строки 
+    char newStr[100]; //создаем новую строку, куда будем записывать исходную наоборот
+    int j = 0;
+    size = strlen(str);
+
+    for (i=size - 1; i>=0; i--) //с помощью цикла переворачиваем исходную строку  
+    {
+        newStr[j] = str[i];
+        j++;
+    }
+    strcpy(str,newStr); //копируем полученную строку в исходную
+}
+
