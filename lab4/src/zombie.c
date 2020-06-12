3acfadd15cda0047eadc6eb0e8c5f7df7b3f4de8
jul-andreevna <yuliaaleks28070@gmail.com>
2020-06-12 09:22:17 +0000

    done

---
+++ lab4/src/zombie.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+
+ pid_t child_pid;
+
+ child_pid = fork();
+
+ if (child_pid > 0) { //усыпляем родительский процесс
+
+  sleep(60);
+
+ } else if (child_pid ==0) {
+  
+  exit(0);
+
+ }
+
+ return 0;
+
+}
